3421ce13b2c9aa34f2fdec6e5a3c0e139b0515bc
Paolo De Donato <dedonato95@hotmail.it>
2021-05-23 22:25:42 +0200

    Endianess

--- src/strsuite/encmetric/base64.hpp
+++ src/strsuite/encmetric/base64.hpp
@@ -1,4 +1,21 @@
 #pragma once
+/*
+    This file is part of Encmetric.
+    Copyright (C) 2021 Paolo De Donato.
+
+    Encmetric is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Encmetric is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Encmetric. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <strsuite/encmetric/encoding.hpp>
 #include <strsuite/encmetric/enc_string.hpp>
@@ -23,11 +40,10 @@ class Base64_padding{
 			}
 			return -1;
 		}
-		static constexpr uint unity() noexcept {return 4;}
+		static constexpr uint min_bytes() noexcept {return 4;}
 		static constexpr bool has_max() noexcept {return true;}
 		static constexpr uint max_bytes() noexcept {return 4;}
-		static constexpr uint chLen(const byte *) {return 4;}
-		static constexpr uint encLen(const three_byte &) {return 4;}
+		static constexpr uint chLen(const byte *, size_t) {return 4;}
 		static bool validChar(const byte *, uint &) noexcept;
 		static uint decode(three_byte *uni, const byte *by, size_t l);
 		static uint encode(const three_byte &uni, byte *by, size_t l);
---
+++ src/strsuite/encmetric/endianess.hpp
@@ -0,0 +1,71 @@
+#pragma once
+/*
+    This file is part of Encmetric.
+    Copyright (C) 2021 Paolo De Donato.
+
+    Encmetric is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Encmetric is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Encmetric. If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <concepts>
+#include <strsuite/encmetric/encoding.hpp>
+
+namespace sts{
+
+template<bool be, typename T, unsigned int N = sizeof(T)> requires std::unsigned_integral<T>
+class Endian_enc{
+public:
+    using ctype=T;
+    static constexpr uint min_bytes() noexcept{ return N;}
+    static constexpr bool has_max() noexcept {return true;}
+    static constexpr uint max_bytes() noexcept{ return N;}
+	static uint chLen(const byte *, size_t siz) {return N;}
+	static validation_result validChar(const byte *, size_t l) noexcept {return validation_result{l >= static_cast<size_t>(N), N};}
+	static uint decode(T *uni, const byte *by, size_t l){
+        if(l < N)
+            throw buffer_small{N - static_cast<uint>(l)};
+        *uni = T{0};
+        if constexpr(be){
+            for(uint i=0; i < N; i++){
+                *uni = (*uni * 0x100u) + static_cast<T>(by[i]);
+            }
+            return N;
+        }
+        else{
+            for(uint i=0; i < N ; i++){
+                *uni = (*uni * 0x100u) + static_cast<T>(by[N - 1 - i]);
+            }
+            return N;
+        }
+    }
+	static uint encode(const T &uni, byte *by, size_t l){
+        if(l < N)
+            throw buffer_small{N - static_cast<uint>(l)};
+        T temp = uni;
+        if constexpr(!be){
+            for(uint i=0; i < N; i++){
+                by[i] = static_cast<byte>(temp & 0xffu);
+                temp /= 0x100u;
+            }
+            return N;
+        }
+        else{
+            for(uint i=0; i < N; i++){
+                by[N - 1 - i] = static_cast<byte>(temp & 0xffu);
+                temp /= 0x100u;
+            }
+            return N;
+        }
+    }
+};
+
+}
